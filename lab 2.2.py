f4e6c381f94571141b3fb6b5abe11c1b8d9a8e80
V1k70R-I <59181199+V1k70R-I@users.noreply.github.com>
2020-04-25 14:57:28 +0300

    Add files via upload

---
+++ lab 2.2.py
@@ -0,0 +1,53 @@
+import random
+import math
+from time import time
+
+
+class Lab3:
+    def __init__(self):
+        self.N = 2400
+        self.w = 1200
+        self.n = 8
+        self.t = [i for i in range(self.N)]
+        self.pi = math.pi
+
+    def count_sin(self):
+        res = []
+        for i in range(self.n):
+            A = random.random()
+            fi = random.random() * 6.28
+            res_i = [A * math.sin(self.w * self.t[i] * i + fi) for i in range(self.N)]
+            res.append(res_i)
+        return res
+
+    def count_x_t(self):
+        res = self.count_sin()
+        x_t = []
+        for i in range(self.N):
+            x = 0
+            for j in range(self.n):
+                x += res[j][i]
+            x_t.append(x)
+        return x_t
+
+    def count_f_x(self, x_t):
+        len_xt = len(x_t)
+        if len_xt <= 1: return x_t
+        even = self.count_f_x(x_t[0::2])
+        odd = self.count_f_x(x_t[1::2])
+        t = [complex(math.cos(-2 * self.pi * i / len(x_t)), math.sin(-2 * self.pi * i / len(x_t))) * odd[i] for
+             i in range(len_xt // 2)]
+        return [even[i] + t[i] for i in range(len_xt // 2)] + [even[i] - t[i] for i in range(len_xt // 2)]
+
+
+    def result(self):
+        x_t = self.count_x_t()
+        time_start1 = time()
+        self.count_f_x(x_t)
+        time_end1 = time()
+        time_res1 = time_end1-time_start1
+        print("Час виконання:", time_res2)
+
+
+
+Lab3().result()
